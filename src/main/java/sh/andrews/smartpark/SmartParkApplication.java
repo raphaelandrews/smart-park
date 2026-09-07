e8083139f21f96a7790619765a8659489feb9b1c
raphaelandrews <hey@andrews.sh>
2026-09-06 21:13:43 -0300

    first commit

---
+++ src/main/java/sh/andrews/smartpark/SmartParkApplication.java
@@ -0,0 +1,13 @@
+package sh.andrews.smartpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkApplication.class, args);
+	}
+
+}
